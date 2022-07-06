1e1fd217f631687086fa630265bf876d355951ed
Yash  Mehta <41582149+myash007@users.noreply.github.com>
2022-07-05 20:02:28 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,5 +8,3 @@ module.exports = {
     // url: "mongodb+srv://" + user + ":" + password + "@cluster0.m71el.mongodb.net/" + database + "?retryWrites=true&w=majority"
     url: "mongodb+srv://" + user + ":" + password + "@cluster0.ona7y.mongodb.net/" + database + "?retryWrites=true&w=majority"
 };
-
-//mongodb+srv://vrushank796:Vrushank796@cluster0.ona7y.mongodb.net/sample_restaurants?retryWrites=true&w=majority
